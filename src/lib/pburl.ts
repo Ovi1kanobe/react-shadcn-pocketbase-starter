9d17e349a6cdaff345c2de8c3e12a80ea03f87e4
Ovi Quintanal <oviquintanal0129@gmail.com>
2025-06-11 17:26:45 +0000

    fixed bug with eye icon duplicated

--- src/lib/pburl.ts
+++ src/lib/pburl.ts
@@ -1,2 +1,2 @@
-const PBURL = "https://example.com";
+const PBURL = "https://befthcc2024.firetailhosting.com";
 export default PBURL;
--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <TooltipProvider delayDuration={0}>
       <ClientContextProvider>
         <AuthContextProvider>
-            <App />
+          <App />
         </AuthContextProvider>
       </ClientContextProvider>
     </TooltipProvider>
