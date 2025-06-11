7554ebe7e8cd911e604792a3374ef07dece7d14d
Ovi1kanobe <94891720+Ovi1kanobe@users.noreply.github.com>
2025-06-11 19:56:32 -0400

    Add account and profile pages

---
+++ src/pages/account.tsx
@@ -0,0 +1,7 @@
+function AccountPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full mt-10 gap-8 p-4">Account</div>
+  );
+}
+
+export default AccountPage;
---
+++ src/pages/profile.tsx
@@ -0,0 +1,7 @@
+function ProfilePage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full mt-10 gap-8 p-4">Profile</div>
+  );
+}
+
+export default ProfilePage;
