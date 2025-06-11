bbb118148f4449f43f89da0dab4964e081a07a18
Ovi1kanobe <94891720+Ovi1kanobe@users.noreply.github.com>
2025-06-11 18:47:33 -0400

    Require PB_URL environment variable

--- src/lib/pburl.ts
+++ src/lib/pburl.ts
@@ -1,2 +1,6 @@
-const PBURL = "https://befthcc2024.firetailhosting.com";
+const PBURL = import.meta.env.PB_URL;
+if (!PBURL) {
+  throw new Error("PB_URL environment variable is not set");
+}
+
 export default PBURL;
