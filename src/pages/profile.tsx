690c07cbf04869accf8a5f0bc3bae0ee7a591397
Ovi Quintanal <admin@hampshirehome.cloud>
2025-06-14 21:11:45 -0400

    added fields for the profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { useAuth } from "@/hooks/useAuth";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import { useClient } from "@/hooks/useClient";
 import { Camera } from "lucide-react";
@@ -13,6 +13,33 @@ function ProfilePage() {
   const { user, updateUser } = useAuth();
   const { pb } = useClient();
   const [name, setName] = useState("");
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClick = () => {
+    fileInputRef.current?.click(); // programmatically open file dialog
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      updateUser(
+        {
+          avatar: file, //Due to Pocketbase Typegen's limitations, we must ignore this type error. The update function can infact take a file
+        },
+        () => {
+          toast.error("Failed to update avatar. Please try again.");
+        },
+        () => {
+          toast.success("Avatar updated successfully!");
+          // Reset the file input
+          if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+          }
+        }
+      );
+    }
+  };
 
   useEffect(() => {
     if (!user) return;
@@ -36,13 +63,26 @@ function ProfilePage() {
     <PageContainer>
       <div className="p-4 pb-2">
         <Card className="p-4 flex items-center gap-4">
-          <Avatar className="size-16 relative group flex items-center justify-center">
+          <Avatar
+            className="size-36 relative group flex items-center justify-center shadow-xl"
+            onClick={handleClick}
+          >
             {user?.avatar && (
               <AvatarImage src={pb.files.getURL(user, user.avatar)} alt={user.name} />
             )}
             <AvatarFallback className="uppercase">{user?.name?.slice(0, 2)}</AvatarFallback>
-            <div className="w-full h-full bg-black absolute cursor-pointer opacity-0 group-hover:opacity-70"></div>
-            <Camera className="absolute text-white hidden group-hover:block cursor-pointer" />
+            <div className="w-full h-full bg-black absolute cursor-pointer opacity-0 group-hover:opacity-50 transition-all duration-300"></div>
+            <Camera
+              size={18}
+              className="absolute text-white opacity-0 group-hover:opacity-100 cursor-pointer transition-all duration-500"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleFileChange}
+              ref={fileInputRef}
+            />
           </Avatar>
           <div>
             <h1 className="text-2xl font-bold leading-none">{user?.name || "Profile"}</h1>
