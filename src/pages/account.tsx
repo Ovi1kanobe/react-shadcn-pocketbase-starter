1664dd82c4b537805b43f104e30c5745a59531ab
Ovi Quintanal <admin@hampshirehome.cloud>
2025-06-15 22:26:45 -0400

    added confirmation box to changing user email.

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,13 +2,26 @@ import EditableTextCard from "@/components/core/editable-text-card";
 import PageContainer from "@/components/core/page-container";
 import { useAuth } from "@/hooks/useAuth";
 import { useClient } from "@/hooks/useClient";
+import { useConfirmDialog } from "@/hooks/useConfirmDialog";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 function AccountPage() {
-  const { user, requestEmailChange, externalAuths } = useAuth();
+  const { user, requestEmailChange, externalAuths, fetchCurrentUser } = useAuth();
   const { pb } = useClient();
+  const confirmDialog = useConfirmDialog();
   const [email, setEmail] = useState("");
+
+  const onConfirmChangeEmail = () => {
+    fetchCurrentUser(
+      () => {
+        toast.error("error fetching user after email change. Please try again.");
+      },
+      () => {
+        // User will be logged out after email change, so we need to redirect to login and make sure the user is aware that they will be logged in.
+      }
+    )
+  }
 
   useEffect(() => {
     if (!user) return;
@@ -22,9 +35,16 @@ function AccountPage() {
         toast.error("Failed to update email. Please try again.");
       },
       () => {
-        toast.success(
-          "Email updated successfully! Please check your inbox for a confirmation email."
-        );
+        confirmDialog.openDialog(
+          {
+            title: "Email Change Requested",
+            description:
+              "A confirmation email has been sent to your new email address. Please check your inbox and follow the instructions to complete the change. Then click OK to once you have confirmed the change.",
+            confirmLabel: "OK",
+            cancelLabel: null,
+            onConfirm: onConfirmChangeEmail
+          }
+        )
       }
     );
   };
