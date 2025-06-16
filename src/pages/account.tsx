ed065f86969c729a33204014c363ff1485a3f0af
Ovi Quintanal <oviquintanal0129@gmail.com>
2025-06-16 13:50:11 +0000

    providers componenent

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,8 +1,15 @@
 import EditableTextCard from "@/components/core/editable-text-card";
 import PageContainer from "@/components/core/page-container";
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
 import { useAuth } from "@/hooks/useAuth";
 import { useClient } from "@/hooks/useClient";
 import { useConfirmDialog } from "@/hooks/useConfirmDialog";
+import { ChevronsUpDown } from "lucide-react";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -11,6 +18,7 @@ function AccountPage() {
   const { pb } = useClient();
   const confirmDialog = useConfirmDialog();
   const [email, setEmail] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const onConfirmChangeEmail = () => {
     fetchCurrentUser(
@@ -60,6 +68,33 @@ function AccountPage() {
         disabled={externalAuths != null && externalAuths.length > 0} // Disable changing email addresses if external auths are linked
         disabledReason="You have linked external authentication methods. Please unlink them before changing your email."
       />
+      <Collapsible
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      className="flex w-[350px] flex-col gap-2"
+    >
+      <div className="flex items-center justify-between gap-4 px-4">
+        <h4 className="text-sm font-semibold">
+          @peduarte starred 3 repositories
+        </h4>
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="icon" className="size-8">
+            <ChevronsUpDown />
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <CollapsibleContent className="flex flex-col gap-2">
+      {
+        externalAuths && externalAuths.map((auth) => (
+          <div className="rounded-md border px-4 py-2 font-mono text-sm">
+            {auth.provider}
+          </div>
+        ))
+      }
+
+      </CollapsibleContent>
+    </Collapsible>
     </PageContainer>
   );
 }
